ad4b6f89f53698892244380de589cc2e6fdfc4db
jusdot <cloud.pye@gmail.com>
2013-03-29 21:23:45 +0800

    hdoj 1723

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
+int arr[40];
 int main()
 {
-	int a, b;
-	
-	while (scanf("%x %x", &a, &b) != EOF)
+	int m, n, i, j;
+	freopen("data.txt", "r", stdin);
+	while (scanf("%d%d", &m, &n) != EOF && m || n)
 	{
-		printf("%d\n", a + b);
+		memset(arr, 0, sizeof(arr));
+		arr[1] = 1;
+
+		for (i = 1; i <= m; i++)
+		{
+			for (j = 1; j <= n && i + j <= m; j++)
+			{
+				arr[i + j] += arr[i];
+			}
+		}
+		printf("%d\n", arr[m]);
 	}
 	return 0;
 }
