4439af36df244a8327bcc451274c96a205866e26
jusdot <cloud.pye@gmail.com>
2013-03-31 09:50:01 +0800

    hdoj 1010

--- Source.cpp
+++ Source.cpp
@@ -1,38 +1,66 @@
 #include <stdio.h>
-#define INF 0x6ffffff
+#include <math.h>
+
+char maze[10][10];
+int N, M, T, dx, dy;
+int isSucc = 0;
+void dfs(int x, int y, int cnt)
+{
+	if (cnt > T)
+		return;
+	if (x == dx && y == dy && cnt == T)
+	{
+		isSucc = 1;
+		return;
+	}
+
+	int temp = (T - cnt) - abs(dx - x) - abs(dy - y);
+	if (temp < 0 || temp % 2 == 1)
+		return;
+
+	maze[x][y] = 'X';
+
+	if (x - 1 >= 0 && maze[x - 1][y] != 'X')
+		dfs(x - 1, y, cnt + 1);
+	if (x + 1 < N && maze[x + 1][y] != 'X')
+		dfs(x + 1, y, cnt + 1);
+	if (y - 1 >= 0 && maze[x][y - 1] != 'X')
+		dfs(x, y - 1, cnt + 1);
+	if (y + 1 < M && maze[x][y + 1] != 'X')
+		dfs(x, y + 1, cnt + 1);
+
+	maze[x][y] = '.';
+}
+
 int main()
 {
-	int T, N, i, a, l, h, ml, mh, j;
 	freopen("data.txt", "r", stdin);
-	scanf("%d", &T);
-	for (i = 1; i <= T; i++)
+	while (scanf("%d%d%d", &N, &M, &T) != EOF && N || M || T)
 	{
-		scanf("%d", &N);
-		int sum = 0;
-		int maxsum = -INF;
-		l = 1;h = 0;
-		for (j = 1; j <= N; j++)
+		int i, j, si, sj, w = 0;
+		isSucc = 0;
+		getchar();
+		for (i = 0; i < N; i++)
 		{
-			scanf("%d", &a);
-			sum += a;
-			h++;
-			if (sum > maxsum)
+			for (j = 0; j < M; j++)
 			{
-				maxsum = sum, ml = l, mh = h;
+				scanf("%c", &maze[i][j]);
+				if (maze[i][j] == 'X')
+					w++;
+				else if (maze[i][j] == 'D')
+					dx = i, dy = j;
+				else if (maze[i][j] == 'S')
+					si = i, sj = j;
 			}
-			if (sum < 0)
-			{
-				sum = 0;
-				if (j < N)
-					l = j + 1, h = j;
-			}
-
-
+			getchar();
 		}
-		
-		printf("Case %d:\n%d %d %d\n", i, maxsum, ml, mh);
-		if (i != T)
-			printf("\n");
+		if (M * N - w > T)
+			dfs(si, sj, 0);
+
+		if (isSucc)
+			printf("YES\n");
+		else
+			printf("NO\n");
 	}
 	return 0;
 }
